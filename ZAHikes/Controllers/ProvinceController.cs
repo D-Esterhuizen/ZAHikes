b2acaefd478c16c2b9f8462887f3c10c80342810
Dylan Esterhuizen <u23668475@tuks.co.za>
2025-02-20 10:57:06 +0200

    DB Migration and Population

---
+++ ZAHikes/Controllers/ProvinceController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZAHikes.Data;
+
+namespace ZAHikes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvinceController : ControllerBase
+    {
+        private readonly ZAHikeDBContext dbContext;
+
+        public ProvinceController (ZAHikeDBContext dBContext)
+        {
+            dbContext = dBContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var province = dbContext.Provinces.ToList();
+
+            return Ok(province);
+        }
+    }
+}
